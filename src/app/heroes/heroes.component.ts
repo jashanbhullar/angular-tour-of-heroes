714f6509e7360f32a716de2b3023c9160f4807f9
jashanbhullar <jsbhullar96@gmail.com>
2018-10-25 17:06:21 +0530

    Second Commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
+  hero: Hero = {
+    id: 1,
+    name: 'Aaryama'
+  };
   constructor() { }
 
   ngOnInit() {
