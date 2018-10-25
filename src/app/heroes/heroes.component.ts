6d4427462bc09e9e351c0ac1f310486444fcb558
jashanbhullar <jsbhullar96@gmail.com>
2018-10-25 17:58:16 +0530

    Observables are here.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,11 +10,12 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
-  constructor() { }
+  constructor(private heroService: HeroService) {   }
 
   ngOnInit() {
+    this.heroes = this.heroService.getHeroes();
   }
 
   onSelect(hero: Hero): void {
